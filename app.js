ec4c5356ed041e8c104ea9043f66cf25b1c720cc
spathak99 <shardoolpathak@gmail.com>
2017-10-21 00:02:27 -0400

    Added

--- app.js
+++ app.js
@@ -16,7 +16,62 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
+var userStore = [];
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+     // store user's address
+     var address = session.message.address;
+     userStore.push(address);
+
+     // end current dialog
+    session.endDialog('Welcome to My Fitness Bot');
+    
+ 
 });
+
+setInterval(function () {
+    var newAddresses = userStore.splice(0);
+    newAddresses.forEach(function (address) {
+
+        console.log('Starting survey for address:', address);
+
+        // new conversation address, copy without conversationId
+        var newConversationAddress = Object.assign({}, address);
+        delete newConversationAddress.conversation;
+
+        // start survey dialog
+        bot.beginDialog(newConversationAddress, 'survey', null, function (err) {
+            if (err) {
+                // error ocurred while starting new conversation. Channel not supported?
+                bot.send(new builder.Message()
+                    .text('This channel does not support this operation: ' + err.message)
+                    .address(address));
+            }
+        });
+
+    });
+}, 5000);
+
+bot.dialog('survey', [
+    function (session) {
+        builder.Prompts.text(session, 'Hello... What\'s your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.choice(session, 'Hi ' + results.response + ', What are your fitness goals?',['Bulking', 'Lean Muscle', 'Losing Weight']);
+    },
+    function (session, results) {
+        session.userData.goals = results.response.text;
+        builder.Prompts.text(session, 'Please enter your height ');
+    },
+    function (session, results) {
+        session.userData.height = results.response;
+        builder.Prompts.text(session, 'Please enter your weight ');
+    },
+    function (session, results) {
+        session.userData.weight = results.response;
+        session.endDialog('Got it! ' + session.userData.name +
+            ', your fitness goal is ' + session.userData.goals +
+            ', your height is,' + session.userData.height + ' and your weight is' + session.userData.weight);
+    }
+]);
