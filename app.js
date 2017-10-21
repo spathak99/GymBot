209ea6b2026b3ac88e6243259c983972e01cab7c
spathak99 <shardoolpathak@gmail.com>
2017-10-20 23:00:50 -0400

    Set Up Restify Server

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 //var express = require('express');
 var builder = require('botbuilder');
+var restify = require('restify');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
 
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector, function (session) {
