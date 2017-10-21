36f2d6e334d6f4ffb3fb86feb6fdac3f2cf74024
Ayush Khandelwal <ayushkhd@gmail.com>
2017-10-21 03:17:51 -0400

    tdee

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var height;
 var inches;
 var feet;
 var gender;
+var ActivityType;
 var goal;
 var age;
 var light = 1.375;
 var moderate = 1.55;
 var active = 1.75;
 var BMR;
+var tdee;
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -63,6 +65,8 @@ setInterval(function () {
 }, 5000);
 
 var choices = ['Bulking', 'Lean Muscle', 'Losing Weight'];
+var activities = ['Light', 'Moderate', 'Active'];
+
 var genders = ['Male', 'Female'];
 bot.dialog('survey', [
     function (session) {
@@ -71,19 +75,25 @@ bot.dialog('survey', [
     function (session, results) {
         session.userData.name = results.response;
         name =  results.response.entity;
-        builder.Prompts.text(session, 'Please enter your age:');
+        builder.Prompts.text(session, 'Hi ' + session.userData.name + ', please enter your age:');
     },
 
     function (session, results) {
         session.userData.age = results.response;
-        age = results.response;
-        builder.Prompts.choice(session, 'Hi, please enter your gender:',genders);
+        age = Number(results.response);
+        builder.Prompts.choice(session, 'Please enter your gender:',genders);
+    },
+
+    function (session, results) {
+        session.userData.gender = results.response;
+        gender = results.response;
+        builder.Prompts.choice(session, 'Enter your estimated activy type',activities);
     },
     
     function (session, results) {
-        session.userData.gender = results.response.entity;
-        gender = results.response;
-        builder.Prompts.choice(session, 'Hi ' + session.userData.name + ', What are your fitness goals?',choices);
+        session.userData.ActivityType = results.response.entity;
+        ActivityType = results.response;
+        builder.Prompts.choice(session, 'What are your fitness goals?',choices);
     },
     function (session, results) {
         session.userData.goals = results.response.entity;
@@ -93,25 +103,37 @@ bot.dialog('survey', [
     function (session, results) {
         session.userData.height = results.response;
         height =  results.response;        
-        inches = 12*height.charAt(0);
-        var tempinches=height.charAt(2);
-        
-        if(height.charAt(3)!='\'')
-        tempinches = (tempinches*10)+height.charAt(3); 
-        inches +=  tempinches;
+        inches = 12*Number(height.charAt(0));
+        var tempinches=Number(height.charAt(2));
+        if(height.charAt(3)!='\''){
+        tempinches = 10+Number(height.charAt(3).valueOf());
+    }
+        inches =  Number(inches+tempinches);
+
+        console.log(inches);
         builder.Prompts.text(session, 'Please enter your weight in pounds: ');
     },
     function (session, results) {
-        session.userData.weight = results.response;
-        weight =  results.response.text;
+        session.userData.weight = results.response; 
+        weight =  Number(results.response);
+        console.log(weight);
         if(gender == genders[0]){
-            bmr = 66 + (0.453592 * weight)*13.7 + (height * 2.54)*5 - (6.8 * age);
+            bmr = 66 + (0.453592 * weight)*13.7 + (inches * 2.54)*5 - (6.8 * age);
         }else{
-            bmr = 665 + (0.453592 * weight)*9.6 + (height * 2.54)*1.8- (4.7 * age);
+            bmr = 665 + (0.453592 * weight)*9.6 + (inches * 2.54)*1.8- (4.7 * age);
         }
+        bmr=Math.floor(bmr);
+        if(ActivityType==activities[0])
+        tdee = bmr*1.375;
+        else if (ActivityType==activities[1])
+        tdee= bmr*1.55;
+        else
+        tdee= bmr *1.725;
+        tdee=Math.floor(tdee);
+        
         session.endDialog('Got it! ' + session.userData.name +
-            ', your Basal Metabolic rate is' + '___ ' + '. Therefore, your Total Daily Engery Expenditure is' 
-            + '___ ' + 'calories');
+            ', your Basal Metabolic rate is ' + Number(bmr) + '. Therefore, your Total Daily Engery Expenditure is ' 
+            + tdee + ' calories');
     }
 
 
